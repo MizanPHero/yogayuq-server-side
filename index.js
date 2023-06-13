da2268db6c16021571d745ce70cef095fb90d7f7
Mizanur Rahman <mizanrxb@gmail.com>
2023-06-13 11:23:32 +0600

    approve deny api added

--- index.js
+++ index.js
@@ -88,7 +88,6 @@ async function run() {
 
 
     //class related apis
-
     app.get('/classes', async (req, res) => {
       const email = req.query.email;
     
@@ -101,7 +100,17 @@ async function run() {
       res.send(result);
     });
     
+    app.get('/allclasses', async (req, res) => {
+      const result = await classesCollection.find({}).toArray();
+      res.send(result);
+    });
 
+    app.get('/class/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await classesCollection.findOne(query);
+      res.send(result);
+    })
 
     app.post('/classes', async (req, res) => {
       const body = req.body;
@@ -109,6 +118,33 @@ async function run() {
       res.send(result)
     })
 
+    app.patch('/class/approve/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          status: 'approved'
+        },
+      };
+
+      const result = await classesCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
+
+
+    app.patch('/class/deny/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          status: 'denied'
+        },
+      };
+
+      const result = await classesCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
 
 
 
